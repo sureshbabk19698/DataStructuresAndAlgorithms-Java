331700775cf0f2a0ea1141ed9bb42137c0a9f0e2
Suresh Babu K <85051490+sureshbabk19698@users.noreply.github.com>
2022-07-13 22:20:29 +0530

    Update ClimbStairs.java

--- src/main/arrays/ClimbStairs.java
+++ src/main/arrays/ClimbStairs.java
@@ -3,17 +3,19 @@ package easy;
 public class ClimbStairs {
 
 	public int climbStairs(int n) {
-		if (n == 1) {
-			return 1;
+		if (n == 1 || n == 2) {
+			return n;
 		}
-		int[] steps = new int[n + 1];
-		steps[1] = 1;
-		steps[2] = 2;
+		int current = 0;
+		int prev2 = 2;
+		int prev1 = 1;
 		for (int i = 3; i <= n; i++) {
-			steps[i] = steps[i - 1] + steps[i - 2];
+			current = prev2 + prev1;
+			prev1 = prev2;
+			prev2 = current;
 		}
 
-		return steps[n];
+		return current;
 	}
 
 	public static void main(String[] args) {
