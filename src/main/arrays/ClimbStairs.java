097ed7447846b0b3b47012b2cea73d196047e63b
Suresh Babu K <85051490+sureshbabk19698@users.noreply.github.com>
2022-07-13 22:11:54 +0530

    Create ClimbStairs.java
    Question : https://leetcode.com/problems/climbing-stairs/
    Difficulty Mode : Easy

---
+++ src/main/arrays/ClimbStairs.java
@@ -0,0 +1,36 @@
+package arrays;
+
+public class ClimbStairs {
+
+	public int climbStairs(int n) {
+		if (n == 1) {
+			return 1;
+		}
+		int[] steps = new int[n + 1];
+		steps[1] = 1;
+		steps[2] = 2;
+		for (int i = 3; i <= n; i++) {
+			steps[i] = steps[i - 1] + steps[i - 2];
+		}
+
+		return steps[n];
+	}
+
+	public static void main(String[] args) {
+		// Fibonacci
+		// Based on the previous step , output is determined
+		ClimbStairs cs = new ClimbStairs();
+		// 1 step + 1 step
+		// 2 step
+		System.out.println(cs.climbStairs(2));
+		// 1 step + 1 step + 1 step
+		// 2 step + 1 step
+		// 1 step + 2 step
+		System.out.println(cs.climbStairs(3));
+		// 1 step + 1 step + 1 step + 1 step
+		// 2 step + 1 step + 2 step
+		// 1 step + 2 step + 1 step
+		// 2 step + 1 step + 1 step
+		System.out.println(cs.climbStairs(4));
+	}
+}
